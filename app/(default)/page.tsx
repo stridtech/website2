aedd45e9a93793bc8c7dd8150b683d100ae5b6bd
Ulrik Strid <ulrik@strid.tech>
2024-11-01 16:47:05 +0100

    Good enough?

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -6,11 +6,15 @@ export const metadata = {
   description: "Page description",
 };
 
+import Image from "next/image";
 import Hero from "@/components/hero-home";
 import BusinessCategories from "@/components/business-categories";
 import FeaturesPlanet from "@/components/features-planet";
 import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
+
+import HeroKeyBridge from "@/public/images/bridge-logo.svg";
+import Stripes from "@/public/images/stripes.svg";
 
 export default function Home() {
   return (
@@ -19,6 +23,30 @@ export default function Home() {
       {/*<BusinessCategories />*/}
       <FeaturesPlanet />
       {/*<LargeTestimonial />*/}
+      {/* Stripes illustration */}
+      <section className="relative">
+        <div
+          className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 transform"
+          aria-hidden="true"
+        >
+          <Image
+            className="max-w-none"
+            src={Stripes}
+            width={768}
+            alt="Stripes"
+            priority
+          />
+        </div>
+        <div className="mx-auto max-w-3xl flex justify-center items-center">
+          <Image
+            className="rounded-full"
+            src={HeroKeyBridge}
+            width={400}
+            height={400}
+            alt="Planet"
+          />
+        </div>
+      </section>
       <Cta />
     </>
   );
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -186,10 +186,10 @@ export default function Footer({ border = false }: { border?: boolean }) {
         </div>
       </div>
 
-      {/* Big text */}
+      {/* Big text 
       <div className="relative -mt-16 h-60 w-full" aria-hidden="true">
         <div className="pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-gradient-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['StridTech'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['StridTech'] after:[text-shadow:0_1px_0_white]"></div>
-        {/* Glow */}
+        {/* Glow *\/}
         <div
           className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2/3"
           aria-hidden="true"
@@ -197,6 +197,7 @@ export default function Footer({ border = false }: { border?: boolean }) {
           <div className="h-56 w-56 rounded-full border-[20px] border-blue-700 blur-[80px]"></div>
         </div>
       </div>
+      */}
     </footer>
   );
 }
